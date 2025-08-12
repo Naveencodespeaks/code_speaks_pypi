75c149c1f75740cf41e903e24289b559dc1a34a4
Naveen Adepu <sainaveenadepu@gmail.com>
2025-08-12 17:23:44 +0530

    modified code

--- code_speaks_python/core.py
+++ code_speaks_python/core.py
@@ -2,7 +2,7 @@ import requests
 
 def say_hello(name: str) -> str:
     """Return a greeting string."""
-    return f"Hello, {name}! Welcome to pydemo."
+    return f"Hello, {name}! Welcome to code_speaks_python."
 
 def fetch_url(url: str) -> str:
     """Fetch content from a URL using requests and return first 200 characters."""
